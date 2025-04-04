6e949ef54c5d7b95aeabb55812c787b90a7ab663
JoSu-Min <skg04063@gmail.com>
2025-04-04 18:45:11 +0900

    11

---
+++ test/src/components/HeaderCom.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HeaderCom({ onLogout, isLoggedIn, user }) {
+    return (<>
+        <Link to="/">HOME</Link> &nbsp;
+        <Link to="/list">LIST</Link> &nbsp;
+        {isLoggedIn ? 
+            <><Link to="/logout" onClick={onLogout}>LOGOUT</Link> {user}님 로그인 상태 </>: 
+            <Link to="/login">LOGIN</Link>
+        }
+        &nbsp;
+        <Link to="/register">REGISTER</Link> &nbsp;
+        <hr/>
+    </>);
+}
+
+export default HeaderCom;
